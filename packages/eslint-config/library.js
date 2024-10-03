1245a0e6880b83d6136ccb320dc59ada8ff49480
HaeMin Yoon <yunhatmi@gachon.ac.kr>
2024-10-04 00:52:22 +0900

    fix: 패키지 폴더는 cjs를 사용할 수 있어야 하도록 변경

--- packages/eslint-config/library.js
+++ packages/eslint-config/library.js
@@ -5,4 +5,7 @@ const project = resolve(process.cwd(), "tsconfig.json");
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
   extends: ["./base"],
+  rules: {
+    "@typescript-eslint/no-var-requires": "off",
+  },
 };
