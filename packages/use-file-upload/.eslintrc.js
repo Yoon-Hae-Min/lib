29369f6733d0779415cb1073589199fd27e1e289
HaeMin Yoon <yunhatmi@gachon.ac.kr>
2024-10-10 17:12:25 +0900

    chore: eslint any 설정 제거

--- packages/use-file-upload/.eslintrc.js
+++ packages/use-file-upload/.eslintrc.js
@@ -1,6 +1,9 @@
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
   root: true,
-  extends: ["@yoonhaemin-lib/eslint-config/library.js"],
-  parser: "@typescript-eslint/parser",
+  extends: ['@yoonhaemin-lib/eslint-config/library.js'],
+  parser: '@typescript-eslint/parser',
+  rules: {
+    '@typescript-eslint/no-explicit-any': 'off',
+  },
 };
