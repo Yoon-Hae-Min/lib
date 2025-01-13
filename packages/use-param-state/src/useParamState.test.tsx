7e2dcf7a1db67abb7d45a0589d79210be526699d
HaeMin Yoon <yunhatmi@gachon.ac.kr>
2025-01-13 23:09:08 +0900

    feat: 초기 진입시 query Param을 바꾸는것이 아닌 set해야 url이 바뀌도록 설정

--- packages/use-param-state/src/useParamState.test.tsx
+++ packages/use-param-state/src/useParamState.test.tsx
@@ -30,26 +30,13 @@ const wrapper = ({ children }: PropsWithChildren) => {
 };
 
 describe('useParamState', () => {
-  it('string 초기값을 쿼리 파라미터에 반영 해야한다.', () => {
+  it('set을 하지 않으면 초기 값을 그대로 반영해야 한다.', () => {
     const initialValue = { param1: 'value1', param2: '2' };
 
     const { result } = renderHook(() => useParamState(initialValue), { wrapper });
 
     // 상태가 반영되었는지 확인
     expect(result.current[0]).toEqual(initialValue);
-
-    // URL이 업데이트되었는지 확인
-    expect(screen.getByTestId('search-params').textContent).toBe('?param1=value1&param2=2');
-  });
-
-  it('초기값이 없으면 쿼리 파라미터에 반영되지 않아야 한다.', () => {
-    const { result } = renderHook(() => useParamState<{ param1?: string }>(), { wrapper });
-
-    // 상태가 반영되었는지 확인
-    expect(result.current[0]).toEqual({});
-
-    // URL이 업데이트되었는지 확인
-    expect(screen.getByTestId('search-params').textContent).toBe;
   });
 
   it('setSearchParams로 빈 쿼리 파라미터를 업데이트 해야한다.', () => {
@@ -83,27 +70,6 @@ describe('useParamState', () => {
 
     // URL이 업데이트되었는지 확인
     expect(screen.getByTestId('search-params').textContent).toBe('?param2=newValue&param3.params4=value4');
-  });
-
-  it('setSearchParams로 기존의 쿼리 파라미터를 업데이트 해야한다.', () => {
-    const { result: result2 } = renderHook(
-      () =>
-        useParamState<{
-          param1: string;
-          param2: string;
-        }>({ param1: 'value1', param2: 'value2' }),
-      { wrapper },
-    );
-
-    act(() => {
-      result2.current[1]({ param1: 'newValue', param2: 'newValue' });
-    });
-
-    // 상태가 반영되었는지 확인
-    expect(result2.current[0]).toEqual({ param1: 'newValue', param2: 'newValue' });
-
-    // URL이 업데이트되었는지 확인
-    expect(screen.getByTestId('search-params').textContent).toBe('?param1=newValue&param2=newValue');
   });
 
   it('setSearchParams는 setter함수로도 업데이트 되어야 한다.', () => {
@@ -150,6 +116,11 @@ describe('useParamState', () => {
     };
 
     const { result } = renderHook(() => useParamState<Partial<typeof initialValue>>(initialValue), { wrapper });
+
+    act(() => {
+      result.current[1](initialValue);
+    });
+
     // 상태가 반영되었는지 확인
     expect(result.current[0]).toEqual(initialValue);
 
--- packages/use-param-state/src/useParamState.ts
+++ packages/use-param-state/src/useParamState.ts
@@ -13,45 +13,18 @@ export type ParamStateSetter<T extends SearchParams> = (action: ParamStateAction
 export const useParamState = <T extends SearchParams>(initialValue?: T, options?: NavigateOptions) => {
   const location = useLocation();
   const navigate = useNavigate();
-  const isFirstRender = useRef(true);
 
   // 현재 URL의 쿼리 파라미터를 파싱하여 객체로 변환
   const searchParams = useMemo<T>(() => {
     const params = new URLSearchParams(location.search);
     const parsedParams = unFlattenObject(Object.fromEntries(params.entries()));
 
-    // initialValue와 merge (params 값이 우선)
-    if (initialValue && isFirstRender.current) {
-      Object.entries(flattenObject(initialValue)).forEach(([key, value]) => {
-        if (value === null || value === undefined) return;
-        params.set(key, value.toString());
-      });
+    if (initialValue && params.size <= 0) {
+      return initialValue;
+    } else {
+      return parsedParams as T;
     }
-
-    return parsedParams as T;
   }, [initialValue, location.search]);
-
-  // 초기값을 쿼리 파라미터에 반영
-  useEffect(() => {
-    if (!initialValue) return;
-    const params = new URLSearchParams(location.search);
-
-    // initialValue의 키와 값을 쿼리 파라미터에 설정
-    Object.entries(flattenObject(initialValue)).forEach(([key, value]) => {
-      if (value === null || value === undefined) return;
-      params.set(key, value);
-    });
-
-    isFirstRender.current = false;
-
-    navigate(
-      {
-        pathname: location.pathname,
-        search: params.toString(),
-      },
-      options,
-    );
-  }, []);
 
   // 쿼리 파라미터를 업데이트하는 함수
   const setSearchParams: ParamStateSetter<T> = useCallback(
