88fbb666d2a3788440034f88612a22d45328fc14
YoonHaeMin <yunhatmi@gachon.ac.kr>
2025-12-14 16:53:22 +0900

    chore: rollup을 사용하여 번들링하는 기존 패키지 방식으로 통일

---
+++ packages/swagger-generator/rollup.config.js
@@ -0,0 +1,36 @@
+import babel from '@rollup/plugin-babel';
+import commonjs from '@rollup/plugin-commonjs';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import terser from '@rollup/plugin-terser';
+import analyze from 'rollup-plugin-analyzer';
+import dts from 'rollup-plugin-dts';
+
+const extensions = ['.js', '.jsx', '.ts', '.tsx'];
+
+export default [
+  {
+    input: 'src/index.ts',
+    output: [
+      {
+        file: './lib/index.js',
+        format: 'cjs',
+      },
+    ],
+    plugins: [
+      nodeResolve({ extensions, preferBuiltins: true }),
+      commonjs(),
+      terser(),
+      analyze(),
+      babel({ extensions, babelHelpers: 'bundled' }),
+    ],
+    external: [/node_modules/],
+  },
+  {
+    input: 'types/index.d.ts',
+    output: {
+      file: './lib/index.d.ts',
+      format: 'es',
+    },
+    plugins: [dts()],
+  },
+];
